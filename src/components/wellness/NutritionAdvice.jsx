3da45d9a3048688b79ec12e72f39a836519bfe01
william hallett <williamhallett1414@gmail.com>
2026-02-06 18:36:38 +0000

    File changes

--- src/components/wellness/NutritionAdvice.jsx
+++ src/components/wellness/NutritionAdvice.jsx
@@ -45,7 +45,7 @@ export default function NutritionAdvice() {
       </div>
 
       {/* Featured Nutrition Cards */}
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-6">
         {featuredArticles.map((article, index) => {
           const Icon = article.icon;
           return (
