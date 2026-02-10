8f444b000d60c4107ce2315fb7a2f0074d129d5e
william hallett <williamhallett1414@gmail.com>
2026-02-10 18:35:52 +0000

    File changes

--- src/components/home/WellnessHub.jsx
+++ src/components/home/WellnessHub.jsx
@@ -8,12 +8,12 @@ export default function WellnessHub() {
     {
       name: 'Workouts',
       image: 'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?w=500&h=300&fit=crop',
-      path: createPageUrl('Wellness')
+      path: createPageUrl('Workouts')
     },
     {
       name: 'Nutrition',
       image: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=500&h=300&fit=crop',
-      path: createPageUrl('Wellness')
+      path: createPageUrl('Nutrition')
     },
     {
       name: 'Bible',
