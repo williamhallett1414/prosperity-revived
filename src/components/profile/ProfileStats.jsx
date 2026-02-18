53b23dc965570ca5a43295a5c5cb734153d90082
william hallett <williamhallett1414@gmail.com>
2026-02-18 15:23:17 +0000

    File changes

--- src/components/profile/ProfileStats.jsx
+++ src/components/profile/ProfileStats.jsx
@@ -9,28 +9,28 @@ export default function ProfileStats({ userProgress, meditationSessions, workout
       label: 'Meditation Minutes',
       value: meditationSessions?.reduce((sum, s) => sum + (s.duration_minutes || 0), 0) || 0,
       unit: 'min',
-      color: 'from-[#AFC7E3] to-[#D9B878]'
+      color: 'from-[#3C4E53] to-[#FD9C2D]'
     },
     {
       icon: Zap,
       label: 'Workouts Completed',
       value: workoutSessions?.length || 0,
       unit: '',
-      color: 'from-[#D9B878] to-[#FD9C2D]'
+      color: 'from-[#FD9C2D] to-[#FAD98D]'
     },
     {
       icon: BookOpen,
       label: 'Journal Entries',
       value: journalEntries?.length || 0,
       unit: '',
-      color: 'from-[#FD9C2D] to-[#D9B878]'
+      color: 'from-[#FAD98D] to-[#FD9C2D]'
     },
     {
       icon: Trophy,
       label: 'Badges Earned',
       value: userProgress?.badges?.length || 0,
       unit: '',
-      color: 'from-[#D9B878] to-[#AFC7E3]'
+      color: 'from-[#3C4E53] to-[#FAD98D]'
     }
   ];
 
